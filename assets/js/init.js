60afd8cf0b4c46e64217afa8742d50315ee6eba5
maxime <maxime_vhw@hotmail.com>
2022-12-20 19:27:30 +0100

    added processSubmit and checkGift

--- assets/js/init.js
+++ assets/js/init.js
@@ -2,6 +2,23 @@
 
 document.addEventListener('DOMContentLoaded', init);
 
+
 function init() {
+    document.querySelector("form").addEventListener("submit", processSubmit);
     console.log('yeaaa buddy');
+}
+
+function processSubmit(e) {
+    e.preventDefault();
+    let code = "";
+    document.querySelectorAll("input[type=text]").forEach(input => {
+        code += input.value;
+    })
+    console.log(code);
+    checkGift(code);
+}
+
+function checkGift(code) {
+    const result = _gifts.filter(gift => gift.code = code);
+    console.log(result);
 }
