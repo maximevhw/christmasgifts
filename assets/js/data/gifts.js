1d8d68b4ff86f85047670970afca80fc833e7c60
maxime <maxime_vhw@hotmail.com>
2022-12-20 19:25:25 +0100

    Added temporary gifts db

---
+++ assets/js/data/gifts.js
@@ -0,0 +1,28 @@
+//https://generate.plus/en/number/length
+
+_gifts = [
+    {
+        code: "6519824022674013",
+        name: "Etentje op kosten maxime",
+        img: null,
+        link: ""
+    },
+    {
+        code: "2769183298208737",
+        name: "Wellness",
+        img: null,
+        link: ""
+    },
+    {
+        code: "4406282047892600",
+        name: "another gift",
+        img: null,
+        link: ""
+    },
+    {
+        code: "3778563364789621",
+        name: "yes",
+        img: null,
+        link: ""
+    }
+]
