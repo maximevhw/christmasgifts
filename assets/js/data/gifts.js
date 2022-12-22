6f43645a67d831ef3bcac830f99ee7f6303468c4
maxime <maxime_vhw@hotmail.com>
2022-12-22 05:20:52 +0100

    inject html to show gift

--- assets/js/data/gifts.js
+++ assets/js/data/gifts.js
@@ -5,24 +5,28 @@ _gifts = [
         code: "6519824022674013",
         name: "Etentje op kosten maxime",
         img: null,
+        desc: "Etentje",
         link: ""
     },
     {
         code: "2769183298208737",
         name: "Wellness",
         img: null,
+        desc: "Etentje",
         link: ""
     },
     {
         code: "4406282047892600",
         name: "another gift",
         img: null,
+        desc: "Etentje",
         link: ""
     },
     {
         code: "3778563364789621",
         name: "yes",
         img: null,
+        desc: "Etentje",
         link: ""
     }
 ]
--- assets/js/init.js
+++ assets/js/init.js
@@ -14,11 +14,27 @@ function processSubmit(e) {
     document.querySelectorAll("input[type=text]").forEach(input => {
         code += input.value;
     })
-    console.log(code);
     checkGift(code);
+    console.log(gift);
 }
 
 function checkGift(code) {
-    const result = _gifts.filter(gift => gift.code = code);
-    console.log(result);
+    const $giftArticle = document.querySelector("#gift");
+    $giftArticle.innerHTML = "";
+
+    const gift = _gifts.find(giftObject => giftObject.code === code);
+
+    if (gift === undefined) {
+        $giftArticle.insertAdjacentHTML("beforeend", `
+        <h2>It seems like you entered an incorrect code.</h2>
+        <img src="../media/gift.webp" ALT="gift not found">
+        `);
+    } else {
+        $giftArticle.insertAdjacentHTML("beforeend", `
+<h2>${gift.name}</h2>
+<img src="${gift.img}" alt="${gift.name}">
+<p>${gift.desc}</p>
+`)
+    }
+    //6519 8240 2267 4013
 }
