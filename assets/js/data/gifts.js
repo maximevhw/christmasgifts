1a5fdfca4ecb48d9fd81ab524b6350e09eca7f51
maxime <maxime_vhw@hotmail.com>
2022-12-22 06:07:07 +0100

    Removed unused "link" property

--- assets/js/data/gifts.js
+++ assets/js/data/gifts.js
@@ -3,30 +3,26 @@
 _gifts = [
     {
         code: "6519824022674013",
-        name: "Etentje op kosten maxime",
+        name: "Restaurant",
         img: "assets/media/restaurant.jpg",
-        desc: "Etentje",
-        link: ""
+        desc: "Lekker uiteten op kosten van Maxime",
     },
     {
         code: "2769183298208737",
         name: "Wellness",
         img: null,
         desc: "Etentje",
-        link: ""
     },
     {
         code: "4406282047892600",
         name: "another gift",
         img: null,
         desc: "Etentje",
-        link: ""
     },
     {
         code: "3778563364789621",
         name: "yes",
         img: null,
         desc: "Etentje",
-        link: ""
     }
 ]
