7252ed27ba3b6992be5c450e7ae64fcf9a5793d0
maxime <maxime_vhw@hotmail.com>
2022-12-25 12:16:31 +0100

    added gifts

--- assets/js/data/gifts.js
+++ assets/js/data/gifts.js
@@ -1,23 +1,34 @@
 //https://generate.plus/en/number/length
+// 16 lang
 
 _gifts = [
     {
+        //julie
         code: "6519824022674013",
         name: "Restaurant",
         img: "assets/media/restaurant.jpg",
         desc: "Lekker uiteten op kosten van Maxime",
     },
     {
+        //mama
         code: "2769183298208737",
         name: "Restaurant",
         img: "assets/media/restaurant.jpg",
         desc: "Lekker uiteten op kosten van Maxime",
     },
     {
+        //thibault
         code: "4406282047892600",
-        name: "Restaurant",
+        name: "Uitje met Maxime?",
         img: "assets/media/restaurant.jpg",
-        desc: "Lekker uiteten op kosten van Maxime",
+        desc: "Lekker uiteten of toch wat extremer en eens gaan airsoften?",
+    },
+    {
+        //Geert marleen
+        code: "8025158332381161",
+        name: "Uitje met Maxime?",
+        img: "assets/media/restaurant.jpg",
+        desc: "Eens lekker uitetenof toch wat extremer en eens gaan airsoften?",
     },
     {
         code: "3778563364789621",
